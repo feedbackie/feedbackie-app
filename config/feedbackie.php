94164cd17345febaeec5adbedd40437485c4bc39
Seriyyy95 <Seriyyy95@gmail.com>
2025-12-02 16:52:39 +0200

    fix: asset url

---
+++ config/feedbackie.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'user_model' => "App\\Models\\User",
+    'site_model' => "Feedbackie\\Core\\Models\\Site",
+    'feedback_model' => "Feedbackie\\Core\\Models\\Feedback",
+    'report_model' => "Feedbackie\\Core\\Models\\Report",
+    'metadata_model' => "Feedbackie\\Core\\Models\\Metadata",
+    'api' => [
+        'reports_middleware' => [],
+        'feedbacks_middleware' => [],
+    ],
+
+    'asset_url' => asset("build/assets/app.js"),
+];
